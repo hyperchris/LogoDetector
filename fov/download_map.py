096080635571404c181cb90edee98d3c6c73aae6
hyperchris <johndoe@example.com>
2015-12-05 22:20:48 -0800

    add parse, download map, to fov

---
+++ fov/download_map.py
@@ -0,0 +1,26 @@
+import time, os, sys
+from common import *
+import urllib2
+
+# sample google map api: http://maps.google.com/maps/api/staticmap?center=34.0200862,-118.2892835&zoom=19&size=900x900&maptype=roadmap
+
+SLEEP_TIME = 0.5
+
+PREFIX = 'http://maps.google.com/maps/api/staticmap?center='
+MIDDLE = '&zoom='
+SUFFIX = '&size=900x900&maptype=roadmap'
+
+# download the map image for this vp
+def download_map(vp_data, scale, res_dir, tp_num, vp_num): 
+	# data struct of vp_data is Vp_data from common
+	imgurl = PREFIX + `vp_data.lat` + ',' + `vp_data.lng` + MIDDLE + `scale` + SUFFIX
+	response = urllib2.urlopen(imgurl) # download map
+	img = response.read()
+	map_path = res_dir + 'res_' + `tp_num` + '_vp_' + `vp_num` + '.png'
+	print 'store img to: ' + map_path
+	imout = open(map_path, 'w')
+	imout.write(img)
+	imout.close()
+	time.sleep(SLEEP_TIME) # sleep for 100 ms
+
+	return map_path
--- fov/fov_detect.py
+++ fov/fov_detect.py
@@ -9,11 +9,14 @@
   - Fov detection result and all result images will be stored in [res_dir]
 '''
 
-import time, os, copy, sys, random, threading
+import os, sys, time
+import parse
+from common import *
+import download_map
  
 RES_DIR = 'res/'
 RES_FILE = 'result.txt'
-MAP_SCALE = 20
+MAP_SCALE = 19
 
 def get_name(impath, name_type): # ge the name or pos from image path
 	ret_val_list = impath.split('/')[-1].split(SPLITTER)
@@ -21,51 +24,6 @@ def get_name(impath, name_type): # ge the name or pos from image path
 		return ret_val_list[0]
 	else:
 		return ret_val_list[-1].split('.')[0]
-
-# Go through the dir to process every image
-def dir_walk(rootDir, indent):
-	print 'processing .....'
-	for dirName, subdirList, fileList in os.walk(rootDir):
-        # print indent + dirName
-		img_num = 0
-		for fname in fileList: # file 
-			if '.jpg' in fname or '.png' in fname or '.JPG' in fname or '.PNG' in fname:
-				img_num += 1
-				if 0 == img_num % 100:
-					print '..' + `img_num` + '..'
-				call_merge(rootDir + fname)
-
-# ------------------------------------------------
-
-class Vp_data: # vp data struct
-	lat = 0.0
-	lng = 0.0
-	bearing = 0.0
-	def __init__(lat_, lng_, bearing_):
-		lat = lat_
-		lng = lng_
-		bearing = bearing_
-
-class Raw_data: # triangulation result with list of vp
-	t_lat = 0.0
-	t_lng = 0.0
-	vp_list = Vp_data[]
-	def __init__(t_lat_, t_lng_):
-		t_lat = t_lat_
-		t_lng = t_lng_
-
-# parse two files. Output Raw_data[] as 'raw_data_list'
-def parse(vp_file, t_file):
-	raw_data_list = (Raw_data)[]
-	vp_data = open(vp_file,'r') # view point
-	t_data = open(t_file,'r') # triagulation results
-	
-	return raw_data_list
-
-# download the map image for this vp
-def download_map(vp_data, scale, tp_num, vp_num): 
-	map_path = ''
-	return map_path
 
 # detect the fov: edge_pt is the list made of end point of eyesight from one angle (step: 1")
 def detect_fov(map_path, bearing):
@@ -106,12 +64,14 @@ if __name__ == '__main__':
 	if not os.path.exists(RES_DIR):
 		os.makedirs(RES_DIR)
 	
-	raw_data_list = parse(vp_file, t_file) # parse to get the triangulation result with all viewpoint (vp) in GPS and bearing
-
+	raw_data_list = parse.parse(vp_file, t_file) # parse to get the triangulation result with all viewpoint (vp) in GPS and bearing
+	
 	for i in range(len(raw_data_list)): # for each t_point
 		num_inside = 0
-		for j in range(len(raw_data_list[i])): # for each vp of the t_point
-			map_path = download_map(raw_data_list[i].vp_list[j], MAP_SCALE, i, j) # download the map image for this vp
+		for j in range(raw_data_list[i].vp_list_len): # for each vp of the t_point
+			map_path = download_map.download_map(raw_data_list[i].vp_list[j], MAP_SCALE, RES_DIR, i, j) # download the map image for this vp
+			'''
+			# TODO:
 			edge_pt = detect_fov(map_path, raw_data_list[i].vp_list[j].bearing) # detect the fov: edge_pt is the list made of end point of eyesight from one angle (step: 1")
 			# return distance vector: [x_offset, y_offset]. The first parameter of the function is input
 			dist_vec = dist_from_gps(raw_data_list[i].vp_list[j].lat, raw_data_list[i].vp_list[j].lng, \
@@ -119,6 +79,9 @@ if __name__ == '__main__':
 			is_inside = draw_fov(map_path, edge_pt, dist_vec) # draw the result and return if the tp is within fov
 			if is_inside:
 				num_inside += 1
+			'''
 		append_file(`i`+ ':' + `num_inside`, RES_DIR + RES_FILE) # write result to file
-
+		# TO REMOVE
+		break # this is temporary (for test)
+	
 	print "done"
